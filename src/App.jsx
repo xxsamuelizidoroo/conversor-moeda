6bb8561b88c7e759e8f48a9ad1853cf6293d6e9f
Samuel Izidoro <130665827+xxsamuelizidoroo@users.noreply.github.com>
2024-03-15 12:47:27 -0300

    Finalizado
    Finalizado

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CurrenyComponents from "./components/CurrencyComponents"
+
+
+function App() {
+ 
+  return (
+    <>
+      <CurrenyComponents />
+    </>
+  )
+}
+
+export default App
